6a1fe16238a3a896d24657f9a700a14b9bfd5741
Joe Stubbs <joestubbs5@gmail.com>
2020-08-17 10:21:10 -0500

    deal with exceptions when unable to generate tapis client.

--- actors/__init__.py
+++ actors/__init__.py
@@ -1,3 +1,9 @@
 from common.auth import get_service_tapy_client
+from common.logs import get_logger
+logger = get_logger(__name__)
 
-t = get_service_tapy_client(tenant_id='master', base_url='https://master.develop.tapis.io')
+try:
+    t = get_service_tapy_client(tenant_id='master', base_url='https://master.develop.tapis.io')
+except Exception as e:
+    logger.error(f'Could not instantiate tapy service client. Exception: {e}')
+    raise e
