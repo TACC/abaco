fc351abc36316bc2c0ead7f37327d4ece4c518ee
Christian R. Garcia <christian.g21@gmail.com>
2021-02-01 13:03:38 -0600

    Merged cron test fixes for snake case.

--- tests/test_cron.py
+++ tests/test_cron.py
@@ -10,6 +10,7 @@ import time
 import requests
 import json
 import cloudpickle
+import datetime
 
 from util import headers, base_url, case, \
     response_format, basic_response_checks, get_actor_id, check_execution_details, \
@@ -18,35 +19,58 @@ from util import headers, base_url, case, \
 
 def test_register_actor_with_cron(headers):
     url = '{}/{}'.format(base_url, 'actors')
-    data = {'image': 'jstubbs/abaco_test', 'name': 'cron_test', 'cronSchedule': '2099-06-21 14 + 6 hours'} 
-    rsp = requests.post(url, data=data, headers=headers)
+    if case == 'camel':
+        cron_field = 'cronSchedule'
+    else:
+        cron_field = 'cron_schedule'
+    # we need to use "tomorrow" because abaco uses UTC which could be the next day compared to this
+    # machine's "now()"
+    tomorrow_dt = datetime.datetime.now() + datetime.timedelta(days=1)
+    tomorrow_str = tomorrow_dt.strftime("%Y-%m-%d")
+    cron_str = f'{tomorrow_str} 23 + 6 hours'
+
+    data = {'image': 'jstubbs/abaco_test', 'name': 'abaco_test_suite_cron', cron_field: cron_str}
+    rsp = requests.post(url, json=data, headers=headers)
     result = basic_response_checks(rsp)
-    assert result['cronSchedule'] == '2099-06-21 14 + 6 hours'
+    assert result[cron_field] == cron_str
 
 def test_register_actor_with_incorrect_cron(headers):
     url = '{}/{}'.format(base_url, 'actors')
-    data = {'image': 'jstubbs/abaco_test', 'name': 'cron_test', 'cronSchedule': '2099-06-21 14 + 6 flimflams'} 
+    if case == 'camel':
+        cron_field = 'cronSchedule'
+    else:
+        cron_field = 'cron_schedule'
+    data = {'image': 'jstubbs/abaco_test', 'name': 'abaco_test_suite_cron', cron_field: '2099-06-21 14 + 6 flimflams'}
     rsp = requests.post(url, data=data, headers=headers)
     #result = basic_response_checks(rsp)
     assert rsp.status_code == 500
 
 def test_update_cron(headers):
-    actor_id = get_actor_id(headers, name='cron_test')
+    actor_id = get_actor_id(headers, name='abaco_test_suite_cron')
     url = '{}/actors/{}'.format(base_url, actor_id)
-    data = {'image': 'jstubbs/abaco_test', 'stateless': False, 'cronSchedule': '2099-09-6 20 + 3 months'}
+    if case == 'camel':
+        cron_field = 'cronSchedule'
+    else:
+        cron_field = 'cron_schedule'
+    data = {'image': 'jstubbs/abaco_test', 'stateless': False, cron_field: '2099-09-6 20 + 3 months'}
     rsp = requests.put(url, data=data, headers=headers)
     result = basic_response_checks(rsp)
-    assert result['cronSchedule'] == '2099-09-6 20 + 3 months'
+    assert result[cron_field] == '2099-09-6 20 + 3 months'
 
 def test_update_cron_switch(headers):
-    actor_id = get_actor_id(headers, name='cron_test')
+    actor_id = get_actor_id(headers, name='abaco_test_suite_cron')
     url = '{}/actors/{}'.format(base_url, actor_id)
-    data = {'image': 'jstubbs/abaco_test', 'stateless': False, 'cronSchedule': '2099-09-6 20 + 3 months', 'cronOn': False}
+    if case == 'camel':
+        cron_field = 'cronSchedule'
+        cron_switch = 'cronOn'
+    else:
+        cron_field = 'cron_schedule'
+        cron_switch = 'cron_on'
+    data = {'image': 'jstubbs/abaco_test', 'stateless': False, cron_field: '2099-09-6 20 + 3 months', cron_switch: False}
     rsp = requests.put(url, data=data, headers=headers)
     result = basic_response_checks(rsp)
     assert result['image'] == 'jstubbs/abaco_test'
-    assert result['cronOn'] == False
-
+    assert result[cron_switch] == False
 
 
 # Clean up
