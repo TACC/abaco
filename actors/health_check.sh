f01e11723c9230815a6af38e824a057a916487bc
Joe Stubbs <joestubbs5@gmail.com>
2021-03-31 20:54:47 -0500

    increase frequency that health checks run. this should help with issues where workers get stuck in REQUESED or SHUTDOWN_REQUESTED status in the db.

--- actors/health_check.sh
+++ actors/health_check.sh
@@ -5,9 +5,9 @@
 # give initial processes some time to launch
 sleep 30 #120
 
-# main loop runs every 10 minutes/20 seconds
+# main loop runs every 30 seconds
 while :
 do
     python3 -u /actors/health.py
-    sleep 600  #20
+    sleep 30 # 600
 done
