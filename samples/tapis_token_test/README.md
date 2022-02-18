2d34c57059bc1c9ed0a261a93038dd68195640b5
Joe Stubbs <joestubbs5@gmail.com>
2022-02-18 14:51:21 -0600

    clean up the tapis_token_test example

--- samples/tapis_token_test/README.md
+++ samples/tapis_token_test/README.md
@@ -1,12 +1,19 @@
 ## List Files Actor Sample ##
-# Image: abacosamples/agave_files_list
+# Image: abacosamples/tapis_token_test
 
-This sample demonstrates how to use the agave client to list files on a storage system.
+This sample demonstrates how to use the Tapis access token injected into the actor container to call an API.
 
 # Example Usage #
 
+1) Register the actor.
 ```bash
-$ curl -H "Content-type: application/json" -H "Authorization: Bearer $tok" -d '{"path": "/home/testuser2/testuser2", "system_id": "Agave-diagnostics-storage-ds"}' $base/actors/v2/$aid/messages
+curl -H "Authorization: Bearer $tok" $base/actors/v2 -d "image=abacosamples/tapis_token_test&token=True" |jq
+```
+
+2) Send the actor a message
+
+```bash
+$ curl -H "Content-type: application/json" -H "Authorization: Bearer $tok" -d '{"runs": 3}' $base/actors/v2/$aid/messages
 ```
 
 
--- samples/tapis_token_test/test_token.py
+++ samples/tapis_token_test/test_token.py
@@ -1,13 +1,20 @@
 from agavepy.actors import get_context, get_client
+import requests
 import time
 import datetime
 import os
 
 try:
-    TOTAL_RUNS = int(os.environ.get('runs', '10'))
+    TOTAL_RUNS = int(os.environ.get('runs', '5'))
 except Exception as e:
     print(f"got exception getting TOTAL_RUNS; defaulting to 10. e:{e}")
     TOTAL_RUNS = 10
+
+verbose = False
+try:
+    verbose = os.environ.get('verbose', False)
+except:
+    print(f"verbose option not set.")
 
 try:
     SLEEP = int(os.environ.get('sleep', '1'))
@@ -21,21 +28,57 @@ print(f"TOTAL_RUNS: {TOTAL_RUNS}; SLEEP: {SLEEP}\n")
 def tapis_call(ag):
     try:
         s = ag.systems.list()
-        print(f"output from systems list: {s}; now: {datetime.datetime.now()}\n")
+        print(f"Call to systems list successful.")
+        if verbose:
+            print(f"output from systems list: {s}; now: {datetime.datetime.now()}\n")
     except Exception as e:
         print(f"systems list error; now: {datetime.datetime.now()}; e: {e}")
 
 
 def main():
     print(f"executing; now: {datetime.datetime.now()}")
+#    print(f"env: {os.environ}")
     context = get_context()
     ag = get_client()
-    print(f"got client; now: {datetime.datetime.now()}")
+#    print(f"got client; now: {datetime.datetime.now()}")
+
+    try:
+        print(f"ag._token: {ag._token}")
+    except Exception as e:
+        print(f"Got exception trying to read ag._token: {e}")
+
+
+    # try:
+    #     print(f"token_info: {ag.token.token_info}")
+    # except Exception as e:
+    #     print(f"Got exception trying to read token_info: {e}")
+
+
+    try:
+        access_token = os.environ.get('_abaco_access_token')
+        print(f"Got abaco access token: {access_token}")
+    except Exception as e:
+        print(f"Could not get access token from abaco; e: {e}")
+
+    # try to use the agavepy client to call tapis --
+    print("\n\n*********\n\n")
     for i in range(TOTAL_RUNS):
         tapis_call(ag)
         time.sleep(SLEEP)
+
+    # try to call Tapis with the abaco token using requests --
+    for i in range(TOTAL_RUNS):
+        print("\n\n*********\n\n")
+        headers = {"Authorization": f"Bearer {access_token}"}
+        try:
+            rsp = requests.get("https://api.tacc.utexas.edu/systems/v2", headers=headers)
+            rsp.raise_for_status()
+            print("got 200 with abaco token!")
+        except Exception as e:
+            print(f"Got exception trying to call tapis with the abaco token. e: {e}")
+
     print(f"exiting, now: {datetime.datetime.now()}")
-    print(f"environment: {os.environ}")
+    print(f"env: {os.environ}")
 
 if __name__ == '__main__':
     main()
